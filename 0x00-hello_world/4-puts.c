27f36aaffc2dec1f6d976cc46e2492ef03413ad0
WellingtonThabiso <thabisodlamini924@gmail.com>
2023-02-16 14:44:10 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main - print a line  of code  using puts 
+ *
+ * return:  0
+ */
+int main(void)
+	puts("\"Programming is like building a multilingual puzzle")
+	rerurn (0):
