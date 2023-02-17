0f389c93702c32a0888f0152f546eb58c687b6a6
WellingtonThabiso <thabisodlamini924@gmail.com>
2023-02-16 19:47:30 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#include <stdio.h>
+
+/**
+ *
+ * main -entry point 
+ * return:Always 0 (success)
+ */
+
+int main(void)
+{
+                printf("with proper grammar, but the outcome is a piece of art") 
+			return (0);
+}		
